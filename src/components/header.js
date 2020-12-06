937bf59be56853db59e8537863c2e8d4feba6b24
Vrinda <vrindagupta6828@gmail.com>
2020-12-06 18:41:03 +0530

    Added github star and fork buttons

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,10 @@ const Header = () => {
 				<NavbarBrand href='./'>GitHub API Playground</NavbarBrand>
 				<button className="btn btn-success" onClick={handleClick}>Compare Users</button>
 			</Navbar>
+			<div>
+			<iframe src="https://ghbtns.com/github-btn.html?user=adityabisoi&repo=github-rest-api-v3&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+            <iframe src="https://ghbtns.com/github-btn.html?user=adityabisoi&repo=github-rest-api-v3&type=fork&count=true&size=large" frameborder="0" scrolling="0" width="158px" height="30px"></iframe>
+            </div>
 		</header>
 	);
 };
